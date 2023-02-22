516d8a5ee5de8682a20fd28098ec7af36a43114f
ABEL K ANTONY <abelkantony98@gmail.com>
2023-02-22 05:50:12 +0000

    update

--- main.cpp
+++ main.cpp
@@ -22,25 +22,68 @@ void ChangeCase(string word, int length)
 	cout<<"\nWORD AFTER CHANGING CASE";
 	cout<<endl<<endl<<word;
 }
+
+//function to create new string with next letter 
+void StringWithNextLetter(string word,int length)
+{
+	int ascii;
+	string newWord = word;
+	for (int i=0; i<length;i++)
+	{
+		ascii = int(word[i]);
+		if(ascii == 90)
+		{
+			ascii = 65;
+			newWord[i] = char(ascii);	
+		}
+		else if(ascii == 122)
+		{
+			ascii =97;
+			newWord[i] = char(ascii);
+		}
+		else
+		{
+			ascii +=1;
+			newWord[i] = char (ascii);
+		}
+	}
+	cout<<"\nOLD WORD = "<<word;
+	cout<<"\nNEW WORD = "<<newWord;
+}
+
+//function to get the cout of duplicate
+void GetCoutOfLetter(string word ,int length)
+{
+	
+}
 	
 int main() 
 {
+	//variables
 	int length;
 	int option;
 	string word;
+	//user input
 	cout<<"\nENTER A WORD : ";
 	cin>>word;
-	cout<<"\nDEFAULT WORD";
-	cout<<"\n\n"<<word;
 	length = word.size();
+	//display options
 	cout<<"\nOPTION";
 	cout<<"\n1. CHANGE CASE";
+	cout<<"\n2. CREATE A NEW STRING WITH NEXT LETTER";
+	cout<<"\n3. GET THE COUT OF EACH CHARACTER PRESENT";
 	cout<<"\nENTER YOUR CHOICE : ";
 	cin>>option;
 	switch(option)
 	{
 		case 1:	
 		ChangeCase( word, length);
+		break;
+		case 2:
+		StringWithNextLetter(word , length);
+		break;
+		case 3:
+		GetCoutOfLetter(word , length);
 		break;
 		default:
 		cout<<"\nINVALID OPTION";
